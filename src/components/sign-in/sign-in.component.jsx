4829927585bef235c25787edd967d47124618bf4
oil <oil@macui-MacBook.local>
2020-03-08 00:29:13 +0900

    sign-in capital

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -36,7 +36,7 @@ class SignIn extends React.Component {
         return(
             <div className = 'sign-in'>
                 <h2>I already have an account</h2>
-                <span>Sign in with your e-mail and password</span>
+                <span>Sign in with your E=MAIL and PASSWORD</span>
 
                 <form onSubmit = {this.handleSubmit}>
                     <FormInput name ='email' 
